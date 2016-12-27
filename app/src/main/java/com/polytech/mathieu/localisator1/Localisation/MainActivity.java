1fd5d93b27698b4ad6eb5f0fbd8f58e80cf94486
Matwing <matwing68@gmail.com>
2016-12-27 01:21:26 +0100

    Ajout du temps dans le json Ajout du serveur pour envoyer le json

---
+++ app/src/main/java/com/polytech/mathieu/localisator1/Localisation/MainActivity.java
@@ -0,0 +1,210 @@
+package com.polytech.mathieu.localisator1.Localisation;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.polytech.mathieu.localisator1.R;
+
+import org.w3c.dom.Text;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "TestGPS";
+    private boolean service_lance = false;
+    public static TextView textView;
+
+    static final int SocketServerPORT = 3333;
+    ServerSocket serverSocket;
+
+    ServerSocketThread serverSocketThread;
+
+    private static final String[] LOCATION_PERMS={
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_NETWORK_STATE
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(LOCATION_PERMS, 15);
+        }
+        textView = (TextView) findViewById(R.id.coordonnees);
+        final Intent intent = new Intent(this, MyService.class);
+
+        final Button buttonStart = (Button) findViewById(R.id.start);
+        final Button buttonStop = (Button) findViewById(R.id.stop);
+        final Button buttonErase = (Button) findViewById(R.id.erase);
+        final Button buttonLaunch = (Button) findViewById(R.id.launch);
+
+        serverSocketThread = new ServerSocketThread();
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                        Log.d(MyService.TAG, "Service lancé!\n");
+                        startService(intent);
+            }
+        });
+
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d(MyService.TAG, "Service arrêté!\n");
+                stopService(intent);
+            }
+        });
+
+        buttonErase.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d(MyService.TAG, "Suppression json!\n");
+                erase();
+            }
+        });
+
+        buttonLaunch.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d(MyService.TAG, "Envoie données!\n");
+                serverSocketThread.start();
+            }
+        });
+    }
+
+
+
+    public void erase(){
+        String fichier = "donnees.json";
+        // Ecriture dans le .json
+
+        //Création du dossier "Coordonnees" à la racine de la mémoire internet du téléphone
+        File mDir = new File(Environment.getExternalStorageDirectory().getPath() + "/Coordonnees");
+        mDir.mkdirs();
+
+        // Création du fichier "donnees.json" dans le dossier "Coordonnees"
+        File mFile = new File(mDir, fichier);
+        try {
+            mFile.createNewFile();
+            FileWriter fileWriter = new FileWriter(mFile, false);
+            fileWriter.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class ServerSocketThread extends Thread {
+
+        @Override
+        public void run() {
+            Socket socket = null;
+            Log.e(TAG, "En attente d'une connexion...\n");
+            try {
+                serverSocket = new ServerSocket(SocketServerPORT);
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                    }});
+
+                while (true) {
+                    socket = serverSocket.accept();
+                    MainActivity.FileTxThread fileTxThread = new MainActivity.FileTxThread(socket);
+                    fileTxThread.start();
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+    }
+
+    public class FileTxThread extends Thread {
+        Socket socket;
+
+        FileTxThread(Socket socket){
+            this.socket= socket;
+        }
+
+        @Override
+        public void run() {
+            File file = new File(
+                    Environment.getExternalStorageDirectory().getPath()+"/Coordonnees",
+                    "donnees.json");
+
+            Log.e(TAG, "Envoie du fichier...\n");
+
+            byte[] bytes = new byte[(int) file.length()];
+            BufferedInputStream bis;
+            try {
+                bis = new BufferedInputStream(new FileInputStream(file));
+                bis.read(bytes, 0, bytes.length);
+                OutputStream os = socket.getOutputStream();
+                os.write(bytes, 0, bytes.length);
+                os.flush();
+                socket.close();
+
+                final String sentMsg = "File sent to: " + socket.getInetAddress();
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this,
+                                sentMsg,
+                                Toast.LENGTH_LONG).show();
+                    }});
+
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/polytech/mathieu/localisator1/Localisation/MyService.java
@@ -0,0 +1,199 @@
+package com.polytech.mathieu.localisator1.Localisation;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static com.polytech.mathieu.localisator1.Localisation.MainActivity.textView;
+import static com.polytech.mathieu.localisator1.R.id.time;
+
+
+/*
+*   Auteur : Mathieu Stackler
+*   Service qui récupère l'heure, la latitude, la longitude et l'altitude de l'utilisateur
+*
+ */
+
+
+public class MyService extends Service {
+
+
+    DateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+    //TAG pour les tests
+    public static final String TAG = "TestGPS";
+    private LocationManager mLocationManager = null;
+
+    // Interval de temps et de distance entre 2 mesures
+    private static final int LOCATION_INTERVAL = 10000; //en ms
+    private static final float LOCATION_DISTANCE = 0f;
+
+
+
+    public class LocationListener implements android.location.LocationListener {
+
+        //Variables pour l'écriture dans le json
+        String fichier = "donnees.json";
+        File mDir = null;
+        File mFile = null;
+
+        //Variables pour les données
+        Location mLastLocation;
+        String donnees;
+        Double altitude;
+        String date;
+
+
+        public LocationListener(String provider) {
+            Log.e(TAG, "LocationListener " + provider);
+            mLastLocation = new Location(provider);
+        }
+
+        //A chaque fois que la localisation change, cette fonction est appelée
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.e(TAG, "onLocationChanged: " + location);
+            mLastLocation.set(location);
+
+            altitude = Location.convert(String.valueOf(location.getAltitude()));
+            Date tmp = new Date(location.getTime());
+            date = format.format(tmp);
+
+            donnees = "Temps," + date +
+                    "Latitude," + mLastLocation.convert(location.getLatitude(), Location.FORMAT_DEGREES) +
+                    ",Longitude," + Location.convert(location.getLongitude(), Location.FORMAT_DEGREES) +
+                    ",Altitude," + altitude +
+                    "\n";
+
+
+            textView.setText(donnees);
+
+            // Ecriture dans le .json
+
+            //Création du dossier "Coordonnees" à la racine de la mémoire internet du téléphone
+            mDir = new File(Environment.getExternalStorageDirectory().getPath() + "/Coordonnees");
+            mDir.mkdirs();
+
+            // Création du fichier "donnees.json" dans le dossier "Coordonnees"
+            mFile = new File(mDir, fichier);
+            try {
+                mFile.createNewFile();
+                FileWriter fileWriter = new FileWriter(mFile, true);
+                fileWriter.write(donnees);
+                fileWriter.close();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.e(TAG, "onProviderDisabled: " + provider);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.e(TAG, "onProviderEnabled: " + provider);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.e(TAG, "onStatusChanged: " + provider);
+        }
+    }
+
+    LocationListener[] mLocationListeners = new LocationListener[]{
+            new LocationListener(LocationManager.GPS_PROVIDER),
+            new LocationListener(LocationManager.NETWORK_PROVIDER)
+    };
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand");
+        super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.e(TAG, "onCreate");
+        initializeLocationManager();
+        try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE,
+                    mLocationListeners[1]);
+        } catch (java.lang.SecurityException ex) {
+            Log.i(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            Log.d(TAG, "network provider does not exist, " + ex.getMessage());
+        }
+        try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE,
+                    mLocationListeners[0]);
+        } catch (java.lang.SecurityException ex) {
+            Log.i(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            Log.d(TAG, "gps provider does not exist " + ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.e(TAG, "onDestroy");
+        super.onDestroy();
+        if (mLocationManager != null) {
+            for (int i = 0; i < mLocationListeners.length; i++) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    mLocationManager.removeUpdates(mLocationListeners[i]);
+                } catch (Exception ex) {
+                    Log.i(TAG, "fail to remove location listners, ignore", ex);
+                }
+            }
+        }
+    }
+
+    private void initializeLocationManager() {
+        Log.e(TAG, "initializeLocationManager");
+        if (mLocationManager == null) {
+            mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+}
